0682906b230179a75074882a8e7a735ca7043d4f
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2024-09-09 18:20:37 +0900

    feature: add set option

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # set up homobrew
 if [ ! -x "$(command -v brew)" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
