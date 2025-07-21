0126fa6a6d9a50be6492f645be016bcdd87bef5a
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2025-07-21 14:58:51 +0900

    which-key setting for showing all keymapping

--- config/nvim/lua/config/plugins/which-key.lua
+++ config/nvim/lua/config/plugins/which-key.lua
@@ -10,6 +10,15 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  keys={
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
   config = function()
     local wk = require('which-key')
     wk.add({
