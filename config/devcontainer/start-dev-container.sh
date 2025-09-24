2902e33ae779352d9cd80a73c7717d925547ee67
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2025-09-25 08:34:39 +0900

    changed dev-container setting

--- config/devcontainer/start-dev-container.sh
+++ config/devcontainer/start-dev-container.sh
@@ -7,26 +7,33 @@ if [ ! -e ./.devcontainer ]; then
   exit 1
 fi
 
+case "$1" in
+"run") ;;
+"help" | *)
+  echo "run: Start dev container"
+  exit 0
+  ;;
+esac
 
 rebuild_flag=""
-case "$1" in
-  "-r"|"--rebuild")
-    rebuild_flag="--remove-existing-container"
-    ;;
-  "-h"|"--help")
-    echo "-r or --rebuild ... rebuild devcontainer"
-    ;;
-  *)
-    ;;
+case "$2" in
+"-r" | "--rebuild")
+  rebuild_flag="--remove-existing-container"
+  ;;
+"-h" | "--help")
+  echo "-r|--rebuild: rebuild devcontainer"
+  exit 0
+  ;;
+*) ;;
 esac
 
 devcontainer up $rebuild_flag \
-    --mount type=bind,source=$(cd ~/.config/nvim/ && pwd -P),target=/nvim-config/nvim/ \
-    --additional-features='{ \
+  --mount type=bind,source="$(cd ~/.config/nvim/ && pwd -P)",target=/nvim-config/nvim/ \
+  --additional-features='{ \
         "ghcr.io/duduribeiro/devcontainer-features/neovim:1": { "version": "stable" }, \
         "ghcr.io/jungaretti/features/ripgrep:1": {}, \
     }' \
-    --workspace-folder .
+  --workspace-folder .
 
 clear
 devcontainer exec --remote-env XDG_CONFIG_HOME=/nvim-config --workspace-folder . zsh
