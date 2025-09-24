427c5cfa863727cb79a2aba98fc4886ba54ba2d2
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2025-09-25 08:32:23 +0900

    add nix setting

---
+++ .zsh/rc/niz.zsh
@@ -0,0 +1,7 @@
+# nixはDemonとして起動している。
+# obsidian://open?vault=Ideastorage&file=Efforts%2FActivity%20Notes%2F%F0%9F%93%94%202025-09-02%20Nix%E3%81%AB%E5%85%A5%E9%96%80%E3%81%99%E3%82%8B
+
+# https://github.com/DeterminateSystems/nix-installer
+export PATH=$PATH:/nix
+
+alias nix-installer=nix-installer
