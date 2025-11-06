cd5abf383257f6b4bd906ea2c3fdf31ccad88155
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2025-11-07 01:32:00 +0900

    WezTermの設定を変更

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,16 +3,46 @@ require('format')
 require('status')
 
 return {
-	font_size = 17.0,
-	font = wezterm.font('HackGen35 Console NF'),
-	color_scheme = "Catppuccin Mocha (Gogh)",
-	window_background_opacity = 0.93,
+  font_size = 16.0,
+  font = wezterm.font_with_fallback {
+    'HackGen35 Console NF'
+  },
+  color_scheme = "Catppuccin Mocha",
 
-	-- Keybindings
-	keys = require('keybinds').keys,
-	key_tables = require('keybinds').key_tables,
-	disable_default_key_bindings = true,
+  window_background_opacity = 0.90,
+  macos_window_background_blur = 20,
 
-	-- Leader key
-	leader = { key = 'w', mods = 'CTRL', timeout_milliseconds = 2000 },
+  use_ime = true,
+  automatically_reload_config = true,
+  ----------------------------------------
+  ---Tab Settings
+  ----------------------------------------
+  -- hide title bar
+  window_decorations = "RESIZE",
+
+  -- Show tab
+  show_tabs_in_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
+
+  -- Tab transparancy
+  window_frame = {
+    inactive_titlebar_bg = "none",
+    active_titlebar_bg = "none",
+  },
+
+  -- Delete tab border line
+  colors = {
+    tab_bar = {
+      inactive_tab_edge = "none",
+    },
+  },
+
+
+  -- Keybindings
+  keys = require('keybinds').keys,
+  key_tables = require('keybinds').key_tables,
+  disable_default_key_bindings = true,
+
+  -- Leader key
+  leader = { key = 'w', mods = 'CTRL', timeout_milliseconds = 2000 },
 }
