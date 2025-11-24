292c6cb8b13929bf06fe02c37917c6242108863e
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2025-11-24 17:33:36 +0900

    Claude CodeでソースをClaude Codeに送信するキーバインドを追加

--- config/nvim/lua/config/plugins/claude-code.lua
+++ config/nvim/lua/config/plugins/claude-code.lua
@@ -3,13 +3,13 @@ return {
   dependencies = {
     "folke/snacks.nvim"
   },
-  config = function ()
+  config = function()
     require("claudecode").setup()
     local keymap = vim.keymap
 
-    keymap.set("t", "<C-,>", "<c-\\><c-n><Cmd>ClaudeCode<cr>", {desc = "Toggle Claude Code"})
-    keymap.set("n", "<C-,>", "<cmd>ClaudeCode<cr>", {desc = "Toggle Claude Code"})
-    keymap.set("n", "<leader>ac", "<cmd>ClaudeCode --continue<cr>", {desc= "Continue Claude Code"})
+    keymap.set("t", "<C-,>", "<c-\\><c-n><Cmd>ClaudeCode<cr>", { desc = "Toggle Claude Code" })
+    keymap.set("n", "<C-,>", "<cmd>ClaudeCode<cr>", { desc = "Toggle Claude Code" })
+    keymap.set("v", "<leader>cs", "<cmd>ClaudeCodeSend<cr>", { desc = "Send Current Line to Claude Code" })
+    keymap.set("n", "<leader>cc", "<cmd>ClaudeCode --continue<cr>", { desc = "Continue Claude Code" })
   end,
 }
-
