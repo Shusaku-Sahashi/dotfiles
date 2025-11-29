09145fe49d3f3d6f7c7724ee963ba50f8f5e1082
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2025-11-29 18:44:47 +0900

    change nvim color scheme

--- config/nvim/lua/config/plugins/colorscheme.lua
+++ config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,45 +1,43 @@
+local TRANSPARENT = true
+
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    local transparent = true -- set to true if you would like to enable transparency
-
-    local bg = "#011628"
-    local bg_dark = "#011423"
-    local bg_highlight = "#143652"
-    local bg_search = "#0A64AC"
-    local bg_visual = "#275378"
-    local fg = "#CBE0F0"
-    local fg_dark = "#B4D0E9"
-    local fg_gutter = "#627E97"
-    local border = "#547998"
-
-    require("tokyonight").setup({
-      style = "night",
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and "transparent" or "dark",
-        floats = transparent and "transparent" or "dark",
-      },
-      on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = transparent and colors.none or bg_dark
-        colors.bg_float = transparent and colors.none or bg_dark
-        colors.bg_highlight = bg_highlight
-        colors.bg_popup = bg_dark
-        colors.bg_search = bg_search
-        colors.bg_sidebar = transparent and colors.none or bg_dark
-        colors.bg_statusline = transparent and colors.none or bg_dark
-        colors.bg_visual = bg_visual
-        colors.border = border
-        colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
-        colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
+  "rebelot/kanagawa.nvim",
+  init = function()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = function(args)
+        if not vim.startswith(args.match, "kanagawa") then
+          return
+        end
+        vim.g.colors_name = args.match
       end,
     })
-
-    vim.cmd("colorscheme tokyonight")
+  end,
+  opts = function()
+    return {
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- Telescope {{
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          -- }}
+          -- Copilot {{
+          CopilotAnnotation = { bg = theme.ui.bg_p1, italic = true },
+          CopilotSuggestion = { bg = theme.ui.bg_p2, italic = true },
+          -- }}
+        }
+      end,
+      globalStatus = true,
+      transparent = TRANSPARENT,
+      compile = true,
+    }
+  end,
+  config = function(_, opts)
+    local k = require("kanagawa").load("dragon")
   end,
 }
