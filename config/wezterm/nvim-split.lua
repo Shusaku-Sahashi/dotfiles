8800d70c4ed1dc0f7a41c1e6a7f438792c5cf261
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2025-11-30 10:39:36 +0900

    refactor: remove unnecessary code

--- config/wezterm/nvim-split.lua
+++ config/wezterm/nvim-split.lua
@@ -19,13 +19,11 @@ M.split_nav = function(resize_or_move, key)
     mods = resize_or_move == 'resize' and 'META' or 'CTRL|SHIFT',
     action = wezterm.action_callback(function(win, pane)
       if is_vim(pane) then
-      	print("hoge")
         -- pass the keys through to vim/nvim
         win:perform_action({
           SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
         }, pane)
       else
-      	print("foo")
         if resize_or_move == 'resize' then
           win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
         else
