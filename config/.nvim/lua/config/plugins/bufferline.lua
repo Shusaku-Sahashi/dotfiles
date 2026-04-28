1bd39a73a9fd4350cce4d096bdd7d6c0b4bb1c84
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2026-04-28 15:04:03 +0900

    t + [ / ] でTabを移動できる様に変更

--- config/.nvim/lua/config/plugins/bufferline.lua
+++ config/.nvim/lua/config/plugins/bufferline.lua
@@ -2,6 +2,12 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
+  keys = {
+    { "t]", "<cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+    { "t[", "<cmd>BufferLineCyclePrev<CR>", desc = "Previous tab" },
+    { "t]", "<cmd>BufferLineCycleNext<CR>", mode = "t", desc = "Next tab" },
+    { "t[", "<cmd>BufferLineCyclePrev<CR>", mode = "t", desc = "Previous tab" },
+  },
   opts = {
     options = {
       mode = "tabs", -- show "tabs" on tab.
