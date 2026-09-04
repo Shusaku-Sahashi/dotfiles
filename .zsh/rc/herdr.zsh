188d52a60fdc3ab1abbf903b714d188179b0ca34
Shusaku-Sahashi <13200387+Shusaku-Sahashi@users.noreply.github.com>
2026-09-05 06:15:29 +0900

    change herdr zsh command

--- .zsh/rc/herdr.zsh
+++ .zsh/rc/herdr.zsh
@@ -2,7 +2,26 @@ alias hss=_herdr-session
 
 function _herdr-session() {
   local selected
-  selected=$(herdr session list | fzf --reverse --header-lines=1 --preview-window=up:wrap | awk '{print $1}')
-  [[ -z "$selected" ]] && return 0
-  herdr session attach "$selected"
+  case "$1" in
+  -a|--attach)
+      selected=$(herdr session list | fzf --reverse --header-lines=1 --preview-window=up:wrap --prompt "attach:" | awk '{print $1}')
+      [[ -z "$selected" ]] && return 0
+      herdr session attach "$selected"
+     ;;
+  -r|-remove) 
+      selected=$(herdr session list | fzf --reverse --header-lines=1 --preview-window=up:wrap --prompt "remove:" | awk '{print $1}')
+      [[ -z "${selected}" ]] && return 0
+      echo "${selected} を削除します。"
+      echo "press any key"
+      read
+
+      herdr session stop "$selected"
+      herdr session delete "$selected"
+    ;;
+  -h|*) 
+      echo "hss <options>"
+      echo "-a, --attach ... attach ssession"
+      echo "-r, --remove ... remove hss session"
+    ;;
+  esac
 }
